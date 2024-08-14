bb241781a962ba2caf30858532bf2fa607493600
John Miller <john.miller@unionstreet.co.uk>
2024-08-14 15:10:20 +0100

    Get queue name from environment

--- Microservice.Order.Function/Helpers/Constants.cs
+++ Microservice.Order.Function/Helpers/Constants.cs
@@ -1,8 +1,8 @@
 ﻿namespace Microservice.Order.Function.Helpers;
 public class Constants
 {
-    public const string AzureServiceBusConnection = "AZURE_SERVICE_BUS_CONNECTION"; 
-    public const string AzureServiceBusQueueOrderHistoryAdded = "order-history-added";
+    public const string AzureServiceBusConnection = "AZURE_SERVICE_BUS_CONNECTION";
+    public const string AzureServiceBusQueueOrderHistoryAdded = "AZURE_SERVICE_BUS_QUEUE_ORDER_HISTORY_ADDED";
 
     public const string FailureReasonValidation = "Validation Errors.";
     public const string FailureReasonInternal = "Internal Error.";
