baa1ce4e8e029dec55076e0eba98be8b21740503
John Miller <john.miller@unionstreet.co.uk>
2024-09-08 14:18:23 +0100

    Get guid from order id message

--- Microservice.Order.Function/Helpers/JsonHelper.cs
+++ Microservice.Order.Function/Helpers/JsonHelper.cs
@@ -8,7 +8,6 @@ public class JsonHelper
     public static T? GetRequest<T>(byte[] message)
     {
         ArgumentNullException.ThrowIfNull(message);
-
         return JsonSerializer.Deserialize<T>(Encoding.UTF8.GetString(message));
     }
 }
